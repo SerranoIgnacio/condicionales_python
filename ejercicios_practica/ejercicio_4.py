2cc5ba9ac8f8a6fcc93f4104aebaa9f2381eb43c
Ignacio Serrano <johndoe@example.com>
2021-11-18 18:54:34 -0300

    Ej 4

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -17,12 +17,26 @@ texto_2 = '7'
 # se utiliza el operador mayor o menor con Strings (textos)
 # Imprima en pantalla según corresponda
 
+if texto_1 > texto_2:
+    print('Texto_1 es mas grande que Texto_2')
+else:
+    print('Texto_2 es mas grande que Texto_1')
+
 # 2-Transforma esas variables tipo texto en variables numéricas con (int)
 # y almacénalas en nuevas variables.
+
+txt_1 = int(texto_1)
+txt_2 = int(texto_2)
+
 # Compare las nuevas variables para ver cual es mayor o menor
 # utilizando los operadores correspondientes
 # ¿Cuál de las nuevas variables es mayor?
 # Imprima en pantalla según corresponda
+
+if txt_1 > txt_2:
+    print('Txt_1 es mas grande que Txt_2')
+else:
+    print('Txt_2 es mas grande que Txt_1')   
 
 # Para pensar!
 # ¿Por qué cree que texto_2 es mayor a texto_1?
